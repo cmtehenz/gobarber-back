e0894f2721ac78647480ca3ee4dd5bea6eabf3dc
Gustavo Costa <cmtehenz@gmail.com>
2020-04-17 11:17:12 -0300

    criando aplicacao inicial

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
+
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parseDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment: Appointment = {
+    id: uuid(),
+    provider,
+    parseDate,
+  };
+
+  appointments.push(appointment);
+
+  return res.json(appointments);
+});
+
+export default appointmentsRouter;
